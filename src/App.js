ad0e41df871ecceeac3e7f38efc41a1193320b8e
Nikita Kushnirov <niki.cushnirov@yandex.ru>
2023-02-25 23:04:27 +0300

    add hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useEffect } from 'react';
 import './App.css';
-
+import { useTelegram } from './hooks/useTelegram';
 
 
 function App() {
+  const {onToggleButton, tg} = useTelegram();
 
   useEffect(() => {
     tg.ready();
@@ -14,7 +15,7 @@ function App() {
 
   return (
     <div className="App">
-
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
