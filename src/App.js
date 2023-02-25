b39bdfca5192740679bd2d04d569cdd5e9074d07
Nikita Kushnirov <niki.cushnirov@yandex.ru>
2023-02-25 23:48:07 +0300

    add form and productlist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import { useEffect } from 'react';
-import './App.css';
 import { useTelegram } from './hooks/useTelegram';
+import {Route, Routes} from 'react-router-dom';
 import Header from './components/Header/Header';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form'
+import './App.css';
 
 function App() {
-  const {onToggleButton, tg} = useTelegram();
+  const {tg} = useTelegram();
 
   useEffect(() => {
     tg.ready();
   }, )
 
-  
-
 
   return (
     <div className="App">
       <Header />
-      <button onClick={onToggleButton}>toggle</button>
+      <Routes>
+        <Route index elemet={<ProductList />} />
+        <Route path={'form'} elemet={<Form />} />
+      </Routes>
     </div>
   );
 }
+
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Button from '../Button/Button'
 import { useTelegram } from '../../hooks/useTelegram';
-
+import './header.css'
 const Header = () => {
     const {user ,onClose} = useTelegram();
 
 
     return (
-        <div className={'header'} >
+        <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>
                 {user?.username}
