b5deb3ada36e71dfc03cfce2d335b27717149b19
Nikita Kushnirov <niki.cushnirov@yandex.ru>
2023-02-26 16:16:21 +0300

    fix

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -30,7 +30,7 @@ const Form = () => {
         tg.MainButton.setParams({
             text: 'Отправить данные'
         })
-    }, [tg])
+    })
 
     useEffect(() => {
         if(!nickname || !elo) {
